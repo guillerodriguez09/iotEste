e0409890093b3a3dff965cb62b1c88253b3eb9b8
Fabricio <118315951+FerkusHD@users.noreply.github.com>
2025-10-26 22:38:07 -0300

    carpeta test

---
+++ src/test/java/com/utec/ioteste/modelos/ControllerTemperaturaTest.java
@@ -0,0 +1,34 @@
+package com.utec.ioteste.modelos;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ControllerTemperaturaTest {
+
+    //private IControllerTemperatura controller;
+
+    @BeforeEach
+    void setUp() {
+        //controller = new ControllerTemperatura();
+        //controller.cargarConfiguracion("src/test/resources/site_config.json");
+    }
+
+    @Test
+    void EncenderTemperaturaBaja() {
+
+    }
+
+    @Test
+    void ApagarTemperaturaAlta() {
+
+    }
+
+    @Test
+    void noSuperarConsumoMaximo() {
+
+    }
+
+    @Test
+    void CagarConfiguracionCorrectamente() {
+
+    }
+}
